32d445e1ca61fa165c6ce2193a4f1a53756da317
BellaDiao <diaolihui@126.com>
2021-10-09 15:02:58 +0800

    ok

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,28 +8,38 @@
 <template>
   <div id="home">
     <div class="topContent" style="display: flex">
-      <!-- <van-area title="" :area-list="areaList" @change="timeFun" /> -->
-      <div style="flex: 5">
-      <div>快捷时间查询</div>
-      <van-radio-group
-        v-model="radio"
-        direction="horizontal"
-        
-      >
-        <van-radio name="1">至今</van-radio>
-        <van-radio name="2">当月</van-radio>
-        <van-radio name="3">当日</van-radio>
-        <van-radio name="4">当年</van-radio>
-      </van-radio-group>
+      <div style="flex: 1">
+        <van-cell is-link @click="showPopup" v-model="carmodel">{{
+          carmodel
+        }}</van-cell>
+        <van-popup
+          v-model="qukshow"
+          position="bottom"
+          :style="{ height: '50%' }"
+        >
+          <van-area
+            :area-list="areaList"
+            ref="myArea"
+            title="请选择"
+            @change="timeFun"
+            @confirm="onConfirm1"
+            @cancel="onCancel1"
+          />
+        </van-popup>
       </div>
-      <div style="flex: 2">
-        <van-cell title="选择日期" :value="date" @click="show = true" style="display:block" />
+      <div style="flex: 4">
+        <van-cell
+          title="选择日期范围"
+          :value="date"
+          @click="show = true"
+          class="title"
+        />
         <van-calendar
+          class="timing"
           v-model="show"
           type="range"
           @confirm="onConfirm"
           :show-confirm="false"
-          style="display:block"
         />
       </div>
     </div>
@@ -37,32 +47,36 @@
       <!-- <van-cell-group inset>
         <van-cell title="总计划" value="1000" />
       </van-cell-group> -->
+      <!-- <div style="width:10px"></div> -->
       <div style="flex: 2" class="groove">
-        <div class="number">总计划</div>
-        <div class="name">122</div>
+        <div class="zjh">总计划</div>
+        <div class="number">122</div>
       </div>
 
-      <div style="flex: 5">
+      <div style="flex: 2.5">
         <div style="float: left">
-          <div>已完成</div>
+          <div class="littleT">已完成</div>
           <div ref="finishChart" style="width: 120px; height: 120px"></div>
         </div>
+      </div>
+      <div style="flex: 2.5">
         <div style="float: left">
-          <div>未完成</div>
+          <div class="littleT">未完成</div>
           <div ref="unfinishChart" style="width: 120px; height: 120px"></div>
         </div>
       </div>
     </div>
+
     <div class="mainContent2">
-      <div ref="barChart" style="width: 200px; height: 150px"></div>
+      <div ref="barChart" style="height: 150px"></div>
     </div>
     <div class="bottomContent">
-      <div>员工计划完成情况</div>
+      <div class="littleT">员工计划完成情况</div>
       <el-table :data="tableData" stripe style="width: 100%">
         <el-table-column prop="xh" label="序号" width="50"> </el-table-column>
         <el-table-column prop="dept" label="部门" width="100">
         </el-table-column>
-        <el-table-column prop="psn" label="工序责任人/分配人" width="70">
+        <el-table-column prop="psn" label="责任人/分配人" width="70">
         </el-table-column>
         <el-table-column prop="finish" label="已完成" width="50">
         </el-table-column>
@@ -75,40 +89,34 @@
     <!-- 数据加载提示gif -->
     <!-- <Loading :show="isShowLoading" /> -->
     <!-- 回到顶部按钮 -->
-    <v-top />
+    <!-- <v-top /> -->
   </div>
 </template>
 
 <script type="text/javascript">
-import { getHomeData } from './../../serve/api/index.js'
 
 // 引入Vuex
 import { mapMutations, mapState } from 'vuex'
 
-import { ADD_TO_CART } from './../../config/pubsub_type.js'
 import $ from 'jquery';
 // 引入页面组件
 import Vue from 'vue';
-import { Area } from 'vant';
+import { Area, Popup } from 'vant';
 import { Calendar } from 'vant';
 import { Cell, CellGroup } from 'vant';
 import * as echarts from 'echarts';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
+import { getHomeData } from './../../serve/api/index.js'
+
 Vue.use(Cell);
 Vue.use(CellGroup);
 Vue.use(Area);
+Vue.use(Popup);
+
 Vue.use(Calendar);
 Vue.use(ElementUI);
-// import Header from './components/header/Header'
-// import Sowing from './components/sowing/Sowing'
-// import Tip from './components/tip/Tip'
-// import Nav from './components/nav/Nav'
-// import VipTip from './components/myVip/VipTip'
-// import FlashBuy from './components/flash/FlashBuy'
-// import SpecialZone from './components/special/SpecialZone'
-// import TabbarGoodsItem from './components/tabbar/TabbarGoodsItem'
-// import Loading from '../../components/loading/LoadingGif'
 
 export default {
   computed: {
@@ -119,41 +127,25 @@ export default {
     this._initData();
   },
   mounted () {
-    $(".van-picker__toolbar").remove();
     this.finishChart();
     this.unfinishChart();
     this.barChart();
   },
   data () {
     return {
-      date: '',
-      show: false,
+      date: '',//时间范围
+      show: false,//时间范围控件是否显示
+      qukshow: false,//快捷时间查询弹框是否显示
+      carmodel: '至今',
       areaList: {
         province_list: {
           110000: '至今',
           120000: '当月',
           130000: '当日',
-          130000: '当年',
-        },
-        // city_list: {
-        //   110100: '北京市',
-        //   120100: '天津市',
-        //   130100: '济南市',
-        //   130101: '烟台市',
-        //   130102: '青岛市',
-        // },
-        // county_list: {
-        //   110101: '东城区',
-        //   110102: '西城区',
-        //   120102: '西城区1',
-        //   120102: '西城区1',
-        //   130102: '开发区',
-        //   130101: '高新区',
-        //   130102: '芝罘区',
-        //   // ....
-        // },
+          140000: '当年',
+        }
       },
-      radio: '1',
+      // radio: '1',
       finishNum: 95,
       unfinishNum: 5,
       totalhNum: 100,
@@ -186,7 +178,47 @@ export default {
         finish: 12,
         unfinish: 12,
         finishR: "12%",
-      }]
+      }, {
+        xh: '1',
+        dept: '上海市普陀区',
+        psn: '王小虎',
+        finish: 12,
+        unfinish: 12,
+        finishR: "12%",
+
+      }, {
+        xh: '1',
+        dept: '上海市普陀区',
+        psn: '王小虎',
+        finish: 12,
+        unfinish: 12,
+        finishR: "12%",
+
+      }, {
+        xh: '1',
+        dept: '上海市普陀区',
+        psn: '王小虎',
+        finish: 12,
+        unfinish: 12,
+        finishR: "12%",
+
+      }, {
+        xh: '1',
+        dept: '上海市普陀区',
+        psn: '王小虎',
+        finish: 12,
+        unfinish: 12,
+        finishR: "12%",
+
+      }, {
+        xh: '1',
+        dept: '上海市普陀区',
+        psn: '王小虎',
+        finish: 12,
+        unfinish: 12,
+        finishR: "12%",
+
+      },]
     }
   },
   // components: {
@@ -205,18 +237,125 @@ export default {
     ...mapMutations(['ADD_GOODS', 'ADD_TO_CART']),
     // 数据初始化
     async _initData () {
+      this.date = this.getTodayTime();//默认查询至今
 
+
+      // 
+      // 
+      // 
+      // const response = await getHomeData();
+      const response = getHomeData;
+      if (response.success) {
+        const data = response.data
+        // 时间列表赋值
+        this.areaList = data.areaList;
+
+        // // 给轮播组件 sowing_list赋值
+        // this.sowing_list = data.list[0].icon_list;
+        // // navList 赋值
+        // this.nav_list = data.list[2].icon_list;
+        // // 给限时抢购赋值
+        // this.flash_sale_product_list = data.list[3].product_list;
+        // // 给Tabbar 商品列表赋值
+        // this.tabbar_all_product_list = data.list[12].product_list;
+        // this.isShowLoading = false
+        // // 给特色专区赋值
+        // this.specialZone = data.special_zone;
+        // // 获取首页广告图
+        // this.home_ad = data.home_ad.image_url;
+      }
     },
-    timeFun () {
+    // 快捷时间查询开始
+    showPopup () {
+      this.qukshow = true;
+    },
+
+    onConfirm1 (val) { //确定选择
+      console.log(val[0].name)
+      this.carmodel = val[0].name;
+
+      switch (this.carmodel) {
+        case '至今':
+          this.date = this.getTodayTime();
+          console.log("至今:", this.date);
+          break;
+        case '当月':
+          this.date = this.getMonthTime();
+          console.log("当月:", this.date);
+
+          break;
+        case '当日':
+          this.date = this.getDayTime();
+          console.log("当日:", this.date);
+          break;
+        case '当年':
+          this.date = this.getYearTime();
+          console.log("当年:", this.date);
+
+          break;
+
+      }
+      this.qukshow = false//关闭弹框
+    },
+    getDayTime () {
+      //获取当日
+      let date = new Date;
+      let y = date.getFullYear()
+      let m = date.getMonth() + 1
+      m = m < 10 ? '0' + m : m
+      let d = date.getDate()
+      d = d < 10 ? ('0' + d) : d
+      return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+    },
+    getMonthTime () {
+      //获取当月
+      let date = new Date;
+      let y = date.getFullYear()
+      let m = date.getMonth() + 1
+      m = m < 10 ? '0' + m : m
+      let d = date.getDate()
+      d = d < 10 ? ('0' + d) : d
+      return `${date.getFullYear()}/${date.getMonth() + 1}`;
+    },
+    getYearTime () {
+      //获取当年
+      let date = new Date;
+      let y = date.getFullYear()
+      let m = date.getMonth() + 1
+      m = m < 10 ? '0' + m : m
+      let d = date.getDate()
+      d = d < 10 ? ('0' + d) : d
+      return `${date.getFullYear()}-${date.getFullYear()}`;
+    },
+    getTodayTime () {
+      //获取至今
+      let date = new Date;
+      let y = date.getFullYear()
+      let m = date.getMonth() + 1
+      m = m < 10 ? '0' + m : m
+      let d = date.getDate()
+      d = d < 10 ? ('0' + d) : d
+      return `${date.getFullYear()}/1/1-${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+    },
+
+    onCancel1 () {//取消选中
+      this.show = false
+      this.$refs.myArea.reset()// 重置城市列表
+    },
+    // 
+    timeFun (val) {
       console.log("快捷时间选择")
     },
+    // 日期控件开始
     formatDate (date) {
-      return `${date.getMonth() + 1}/${date.getDate()}`;
+      return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
     },
     onConfirm (date) {
       const [start, end] = date;
       this.show = false;
       this.date = `${this.formatDate(start)} - ${this.formatDate(end)}`;
+      this.carmodel='查询';
+      console.log("this.date", this.date)
     },
     finishChart () {
       var that = this;
@@ -228,7 +367,7 @@ export default {
           textStyle: {
             fontWeight: 'bold',
             fontFamily: 'Microsoft YaHei',
-            fontSize: 24
+            fontSize: 20
           },
           //标题位置
           top: '5%',
@@ -247,23 +386,22 @@ export default {
               text: '' + value + '个',
               textAlign: 'center',
               fill: '#000',
-              fontSize: 28
+              fontSize: 20
             }
           },
           //第二行文字
           //内容 + 位置
-          {
-            type: 'text',
-            left: 'center',
-            top: '55%',
-            style: {
-              //maxValue进度条最大值
-              text: '共' + maxValue + '个',
-              textAlign: 'center',
-              fill: '#999',
-              fontSize: 24
-            }
-          }
+          // {
+          //   type: 'text',
+          //   left: 'center',
+          //   top: '55%',
+          //   style: {
+          //      text: '共' + maxValue + '个',
+          //     textAlign: 'center',
+          //     fill: '#999',
+          //     fontSize: 24
+          //   }
+          // }
         ],
         series: [{
           type: 'pie',
@@ -290,7 +428,7 @@ export default {
               value: value,
               itemStyle: {
                 normal: {
-                  color: 'white'
+                  color: '#73A0FA'
                 }
               }
             },
@@ -299,7 +437,7 @@ export default {
               value: maxValue - value,
               itemStyle: {
                 normal: {
-                  color: '#73A0FA'
+                  color: 'white'
                 }
               }
             }
@@ -322,7 +460,7 @@ export default {
           textStyle: {
             fontWeight: 'bold',
             fontFamily: 'Microsoft YaHei',
-            fontSize: 24
+            fontSize: 20
           },
           //标题位置
           top: '5%',
@@ -341,23 +479,22 @@ export default {
               text: '' + value + '个',
               textAlign: 'center',
               fill: '#000',
-              fontSize: 28
+              fontSize: 20
             }
           },
           //第二行文字
           //内容 + 位置
-          {
-            type: 'text',
-            left: 'center',
-            top: '55%',
-            style: {
-              //maxValue进度条最大值
-              text: '共' + maxValue + '个',
-              textAlign: 'center',
-              fill: '#999',
-              fontSize: 24
-            }
-          }
+          // {
+          //   type: 'text',
+          //   left: 'center',
+          //   top: '55%',
+          //   style: {
+          //      text: '共' + maxValue + '个',
+          //     textAlign: 'center',
+          //     fill: '#999',
+          //     fontSize: 24
+          //   }
+          // }
         ],
         series: [{
           type: 'pie',
@@ -384,7 +521,7 @@ export default {
               value: value,
               itemStyle: {
                 normal: {
-                  color: 'white'
+                  color: 'red'
                 }
               }
             },
@@ -393,7 +530,7 @@ export default {
               value: maxValue - value,
               itemStyle: {
                 normal: {
-                  color: '#73A0FA'
+                  color: 'white'
                 }
               }
             }
@@ -435,6 +572,7 @@ export default {
             name: '已完成',
             type: 'bar',
             stack: 'total',
+            barWidth: 30,
             label: {
               show: true
             },
@@ -491,24 +629,22 @@ html * {
   background-color: rgb(17, 142, 234);
   padding-top: 10px;
 }
-// .van-picker__toolbar {
-//   display: none !important;
-// }
 
 .groove {
   border-style: groove;
   height: 138px;
   width: 150px;
-  background-color: red;
+  background-color: #e4f4f4;
 }
 .topContent {
-  height: 3.5rem;
+  height: 2rem;
 }
 .mainContent1 {
   height: 8rem;
 }
 .mainContent2 {
-  height: 8rem;
+  height: 9.5rem;
+  width: 100%;
 }
 .bottomContent {
   height: 8rem;
@@ -516,10 +652,27 @@ html * {
 .el-table .el-table__cell {
   padding: 4px 0 !important;
 }
-.van-cell{
+.van-cell {
   padding: 5px 3px !important;
 }
-.van-cell__value{
-  text-align: left;
+.zjh,
+.number {
+  text-align: center;
+  margin-top: 28px;
+  font-size: 24px !important;
 }
+.littleT {
+  text-align: center;
+  font-size: 16px !important;
+}
+.el-table th.el-table__cell > .cell {
+  padding-left: 0px !important;
+  padding-right: 0px !important;
+}
+.van-cell__value {
+  text-align: left !important;
+}
+// .timing{
+
+// }
 </style>
